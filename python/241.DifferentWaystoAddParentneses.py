fd95fecfcae44d0da5d359d342c0c07faa7e2cf2
xycai <tifosi.cai@gmail.com>
2015-08-25 23:54:26 +0800

    241lambda

--- python/241.DifferentWaystoAddParentneses.py
+++ python/241.DifferentWaystoAddParentneses.py
@@ -6,6 +6,8 @@ class Solution(object):
 		:type input: str
 		:rtype: List[int]
 		"""
+		def calc(a, b, op):
+			return {'+':lambda x,y:x+y, '-':lambda x,y:x-y, '*':lambda x,y:x*y}[op](a,b)
 
 		def dfs(ops, nums):
 			#print len(ops)
@@ -18,18 +20,21 @@ class Solution(object):
 			for index, ele in enumerate(ops):
 				left = dfs(ops[0:index], nums[0:index+1])
 				right = dfs(ops[index+1:], nums[index+1:])
-				if ops[index] == '+':
-					for l in left:
-						for r in right:
-							ret.append(l+r)
-				elif ops[index] == '-':
-					for l in left:
-						for r in right:
-							ret.append(l-r)
-				else:
-					for l in left:
-						for r in right:
-							ret.append(l*r)
+				# if ops[index] == '+':
+				# 	for l in left:
+				# 		for r in right:
+				# 			ret.append(l+r)
+				# elif ops[index] == '-':
+				# 	for l in left:
+				# 		for r in right:
+				# 			ret.append(l-r)
+				# else:
+				# 	for l in left:
+				# 		for r in right:
+				# 			ret.append(l*r)
+				for l in left:
+					for r in right:
+						ret.append(calc(l,r,ops[index]))
 			return ret
 
 		input = re.split(r'(\D)', input)
